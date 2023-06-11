dd2036a26e331bf669bc077f5b861f1068a47acc
Syed Mansoor <imaco.mansoor@gmail.com>
2023-06-12 00:18:20 +0530

    added simple table view

--- Reecle/ViewController/HomeViewController.swift
+++ Reecle/ViewController/HomeViewController.swift
@@ -7,14 +7,34 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate {
     
+    var categories : [String] = ["Apple","Mango","Bananna"]
+    @IBOutlet weak var categoryTable: UITableView!
+        
     private let gadgetController = GadgetController(service: GadgetCategoryManager())
     
     override func viewDidLoad() {
         super.viewDidLoad()
         gadgetController.fetchCategories()
+        categoryTable.delegate = self
+        categoryTable.dataSource = self
     }
 
     
+}
+
+extension HomeViewController : UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = categories[indexPath.row]
+        return cell
+    }
+
 }
